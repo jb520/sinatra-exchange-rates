010678d38edb94b7f5179d3aa1e9d9b9a9532fe6
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-03-06 21:40:09 +0000

    initial

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+
+  erb(:home)
+end
+
+get("/:currency") do
+  @currency = params.fetch("currency")
+
+  erb(:currency_list)
+end
+
+get("/:currency/:compare") do
+  @currency = params.fetch("currency")
+  @compare = params.fetch("compare")
+
+  erb(:currency_compare)
 end
