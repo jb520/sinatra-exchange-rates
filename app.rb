78b6ded952d4c5be8f0b724c9cd8936bae57dd28
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-03-07 22:05:16 +0000

    update content

--- app.rb
+++ app.rb
@@ -7,6 +7,10 @@ get("/") do
   raw_data = HTTP.get(api_url)
   raw_data_string = raw_data.to_s
   parsed_data = JSON.parse(raw_data_string)
+  
+  currency_hash = parsed_data.fetch("currencies")
+
+  @currency_arr = currency_hash.keys
 
   erb(:home)
 end
@@ -14,6 +18,14 @@ end
 get("/:currency") do
   @currency = params.fetch("currency")
 
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+  
+  currency_hash = parsed_data.fetch("currencies")
+
+  @currency_arr = currency_hash.keys
   erb(:currency_list)
 end
 
