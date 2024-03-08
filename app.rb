b84f69f0b75b672bb0767879c49bd60de66f2021
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-03-08 20:28:42 +0000

    fix pathing bug

--- app.rb
+++ app.rb
@@ -33,5 +33,10 @@ get("/:currency/:compare") do
   @currency = params.fetch("currency")
   @compare = params.fetch("compare")
 
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+
   erb(:currency_compare)
 end
